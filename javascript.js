c7e2596824c96f066563a05d2868bd71aecbb06e
rambimbaj <107889088+rambimbaj@users.noreply.github.com>
2023-12-13 20:01:15 +0100

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,23 @@
-let compChoice = ;
 function getComputerChoice() {
-
+    const array = ["kő", "papír", "olló"];
+        let compChoice = array[Math.floor(Math.random()*3)];
+        return compChoice;
 }
+
+function playRound() {
+      if ((playerSelection == "kő" && computerSelection === "olló") ||
+         (playerSelection == "papír" && computerSelection === "kő") ||
+         (playerSelection == "olló" && computerSelection === "papír")) {
+            console.log("Nyertél!");
+         } else if 
+            (playerSelection == computerSelection) {
+            console.log("Döntetlen!"); 
+            } else          
+                console.log("Vesztettél!");
+         }
+
+
+const playerSelection = prompt("kő, papír, olló?").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playerSelection, computerSelection);
+playRound();
