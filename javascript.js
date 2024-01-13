1a1cd4c565042928d41d894ddac21fcdf153b762
rambimbaj <107889088+rambimbaj@users.noreply.github.com>
2024-01-13 17:56:13 +0100

    Create 'new game' functionality

--- javascript.js
+++ javascript.js
@@ -15,7 +15,6 @@ function game() {
       playerScore++;
     } else if (playerSelection == computerSelection) {
     } else computerScore++;
-
     const choice = document.createElement("div");
     choice.innerText =
       "Player chose: " +
@@ -27,14 +26,30 @@ function game() {
     document.body.appendChild(choice);
 
     const score = document.createElement("div");
-    score.innerText =
-      "Current score: " + playerScore + " - " + computerScore;
+    score.innerText = "Current score: " + playerScore + " - " + computerScore;
     document.body.appendChild(score);
 
-    console.log(playerSelection, computerSelection);
-    console.log(playerScore, computerScore);
-  }
+    const result = document.createElement("div");
+    if (playerScore == 3) {
+      result.innerText = "Win!";
+      document.body.appendChild(result);
+    } else if (computerScore == 3) {
+      result.innerText = "Lose!";
+      document.body.appendChild(result);
+    } 
 
+    if ((playerScore == 3) || (computerScore == 3)) {
+      const newGame = document.createElement("button");
+      newGame.setAttribute("id", "newGame");
+      newGame.innerText = "Play again!";
+      document.body.appendChild(newGame);
+      document.getElementById("newGame").addEventListener("click", function () {
+      location.reload();
+      })
+    }
+    //   console.log(playerSelection, computerSelection);
+    //   console.log(playerScore, computerScore);
+  }
   document.querySelector("#rock").addEventListener("click", function () {
     playRound("rock");
   });
@@ -44,12 +59,6 @@ function game() {
   document.querySelector("#scissors").addEventListener("click", function () {
     playRound("scissors");
   });
-
-  if (playerScore > computerScore) {
-    console.log("Win!");
-  } else if (playerScore < computerScore) {
-    console.log("Lose!");
-  } else console.log("Tie!");
 }
 playerScore = 0;
 computerScore = 0;
